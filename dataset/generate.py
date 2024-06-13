5bbbb965c4175672e0d5c0a35ed3abfdd12fb133
Alan Freire <alan.freire.87@gmail.com>
2024-06-13 11:29:09 -0300

    Fix: valor padrão do lux aleatório

--- dataset/generate.py
+++ dataset/generate.py
@@ -1,9 +1,9 @@
 import random
 
-luxVal = 0 # Valor de luminosidade
-tempVal = 0 # Valor de temperatura
-humVal = 0 # Valor de umidade
-environmentLuxAverage = 200 # Média de luminosidade
+# Variáveis de controle
+luxVal = 0 # Valor de luminosidade (float)
+tempVal = 0 # Valor de temperatura (float)
+humVal = 0 # Valor de umidade (float)   
 environmentTempAverage = 24  # Média de temperatura
 lightingSwitch = 0 # 0 = desligado, 1 = ligado
 climateSwitch = 0 # 0 = desligado, 1 = ligado
@@ -13,29 +13,36 @@ tempStatus = 0 # 0 = desligado, 1 = ligado
 humStatus = 0 # 0 = desligado, 1 = ligado
 label = 0 # 0 = normal, 1 = notificar iluminação, 2 = notificar ar condicionado
 
-# Criar uma função que gera dados aleatórios para os sensores
-# e determina o rótulo baseado nas condições dos sensores
-# Para valores com rotulo normal, os dados dos sensores devem está dentro dos seguintes intervalos:
-# lux_values de 200 a 1000
-# humidity_values de 30 a 70
-# temperature_values de 15 a 30
-# Para valores com rótulo notificar iluminação:
-# lux_values menor que 200
-# Para valores com rótulo notificar ar condicionado:
-# temperature_values maior que 30
 
+
+"""
+    Gera um rótulo aleatório para os dados
+"""
 def generateLabel():
     return random.randint(0, 2)
 
+def getEnvironmentLux():
+    # Lista de valores de média de ambientes
+    environmentLuxAverageList = [200, 250, 300, 350, 400, 450]
+    # Seleção aleatória do valor da luminosidade média do ambiente
+    return environmentLuxAverageList[random.randint(0, len(environmentLuxAverageList)-1)]
+
+"""
+    Gera um valor de luminosidade aleatório
+"""
 def generateLux(label):
+    environmentLuxAverage = getEnvironmentLux()
     variation = environmentLuxAverage * 0.1
     luxStatus = 1 if label == 1 else 0
 
     if label != 1:
-        return random.uniform(environmentLuxAverage - variation, environmentLuxAverage + variation), luxStatus
+        return random.uniform(environmentLuxAverage - variation, environmentLuxAverage + variation), luxStatus, environmentLuxAverage
     
-    return random.uniform(0, environmentLuxAverage - variation - 1), luxStatus
+    return random.uniform(0, environmentLuxAverage - variation - 1), luxStatus, environmentLuxAverage
 
+"""
+    Gera um valor de umidade aleatório
+"""
 def generateHum(label):
     humStatus = 1 if label == 2 else 0
     if label != 2:
@@ -43,6 +50,9 @@ def generateHum(label):
     
     return random.uniform(50, 80), humStatus
 
+"""
+    Gera um valor de temperatura aleatório
+"""
 def generateTemp(label):
     variation = environmentTempAverage * 0.25
     tempStatus = 1 if label == 2 else 0
@@ -52,9 +62,12 @@ def generateTemp(label):
     
     return random.uniform(environmentTempAverage + variation + 1, 40), tempStatus
 
+"""
+    Gera os dados para os sensores
+"""
 def generateData():
     label = generateLabel()
-    luxVal, luxStatus = generateLux(label)
+    luxVal, luxStatus, environmentLuxAverage  = generateLux(label)
     humVal, humStatus = generateHum(label)
     tempVal, tempStatus = generateTemp(label)
 
@@ -70,7 +83,7 @@ def generateData():
     return (luxVal, humVal, tempVal, lightingSwitch, climateSwitch, doorActuator, luxStatus, humStatus, tempStatus, environmentTempAverage, environmentLuxAverage, label)
 
 
-def generateDataset(num_samples):
+def generateDataset(num_samples): 
     data = []
     for _ in range(num_samples):
         data.append(generateData())
@@ -84,6 +97,6 @@ def saveDataset(data):
 
     print("Arquivo CSV gerado com sucesso.")
 
-data = generateDataset(1000000)
+data = generateDataset(10)
 saveDataset(data)
 print("Algoritmo de geração de dados finalizado.")
