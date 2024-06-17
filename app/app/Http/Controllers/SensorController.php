9c47aba481d0580698c28ff23b13a6b5c279480a
Alan Freire <alan.freire.87@gmail.com>
2024-06-17 15:01:30 -0300

    MQTT command

--- app/app/Http/Controllers/SensorController.php
+++ app/app/Http/Controllers/SensorController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreSensorRequest;
 use App\Http\Requests\UpdateSensorRequest;
 use App\Models\Sensor;
+use PhpMqtt\Client\Facades\MQTT;
 
 class SensorController extends Controller
 {
     public function toggleStatus(UpdateSensorRequest $request, Sensor $sensor)
     {
+        $mqtt = [
+            'air' => [
+                'topic' => ['cmnd/IoT-UFC/arcond/irsend'],
+                'cmd' => [
+                    '{\"Protocol\":\"COOLIX\",\"Bits\":24,\"Data\":\"0xB27BE0\",\"DataLSB\":\"0x4DDE07\",\"Repeat\":0,\"IRHVAC\":{\"Vendor\":\"COOLIX\",\"Model\":-1,\"Mode\":\"Cool\",\"Power\":\"Off\",\"Celsius\":\"On\",\"Temp\":17,\"FanSpeed\":\"Auto\",\"SwingV\":\"Off\",\"SwingH\":\"Off\",\"Quiet\":\"Off\",\"Turbo\":\"Off\",\"Econo\":\"Off\",\"Light\":\"Off\",\"Filter\":\"Off\",\"Clean\":\"Off\",\"Beep\":\"Off\",\"Sleep\":-1}}',
+                    '{\"Protocol\":\"COOLIX\",\"Bits\":24,\"Data\":\"0xB2BF00\",\"DataLSB\":\"0x4DFD00\",\"Repeat\":0,\"IRHVAC\":{\"Vendor\":\"COOLIX\",\"Model\":-1,\"Mode\":\"Cool\",\"Power\":\"On\",\"Celsius\":\"On\",\"Temp\":17,\"FanSpeed\":\"Auto\",\"SwingV\":\"Off\",\"SwingH\":\"Off\",\"Quiet\":\"Off\",\"Turbo\":\"Off\",\"Econo\":\"Off\",\"Light\":\"Off\",\"Filter\":\"Off\",\"Clean\":\"Off\",\"Beep\":\"Off\",\"Sleep\"']],
+            'light' => [
+                'topic' => ['cmnd/IoT-UFC/ligth/power'],
+                'cmd' => ['ON', 'OFF'],
+            ],
+            'door' => [
+                'topic' => ['cmnd/IoT-UFC/door/power'],
+                'cmd' => ['ON', 'OFF'],
+            ],
+        ];
+
         try {
+            MQTT::publish($mqtt[$sensor->slug]['topic'], $mqtt[$sensor->slug]['cmd'][$request->status == 1? 0: 1]);
             $sensor->update(['status' => $request->status]);
-            // Adicionar o comando para mensagem do MQTT
             return to_route('home');
         } catch (\Exception $e) {
             return back()->with('error', $e->getMessage());
