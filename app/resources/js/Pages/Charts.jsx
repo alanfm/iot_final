e6e73440c0a76d48df75ae53f9386ce5a2943995
Alan Freire <alan.freire.87@gmail.com>
2024-07-04 19:51:40 -0300

    Fixes

--- app/resources/js/Pages/Charts.jsx
+++ app/resources/js/Pages/Charts.jsx
@@ -138,7 +138,16 @@ export default function Charts({lux, hum, temp}) {
             <Head title="Home" />
             <div className="flex flex-col w-screen h-screen gap-4 text-gray-600 bg-slate-200">
                 <div className="flex flex-col bg-white shadow-md">
-                    <div className="flex-1 w-full p-4 text-xl font-semibold text-center">Gerenciamento de Sensores</div>
+                    <div className="flex-1 w-full p-4">
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-10 w-10" viewBox="0 0 16 16">
+                                <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5M3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.6 26.6 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.93.93 0 0 1-.765.935c-.845.147-2.34.346-4.235.346s-3.39-.2-4.235-.346A.93.93 0 0 1 3 9.219zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a25 25 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25 25 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135"/>
+                                <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2zM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5"/>
+                            </svg>
+                        </div>
+                        <h1 className="text-center text-xl font-semibold">Gerenciamento de Ambientes</h1>
+                        <h2 className="text-center text-lg">Smart Environment</h2>
+                    </div>
                     <div className="flex flex-wrap">
                         <Link href="/" className="flex justify-center flex-1 pb-2 border-b-2 border-transparent">
                             <span >
